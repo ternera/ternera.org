184101a6dd59ec1deed01a32c9493eadd67dac7c
ternera <hi@ternera.org>
2024-12-31 16:01:30 -0600

    reverting back

--- js/scripts.js
+++ js/scripts.js
@@ -43,19 +43,4 @@ $(document).ready(function() {
             });
         });
     });
-
-        // Theme Toggle
-        const themeToggle = document.getElementById('theme-toggle');
-        const currentTheme = localStorage.getItem('theme') || 'light';
-        document.documentElement.setAttribute('data-theme', currentTheme);
-      
-        themeToggle.addEventListener('click', function() {
-          let theme = document.documentElement.getAttribute('data-theme');
-          if (theme === 'light') {
-            theme = 'dark';
-          } else {
-            theme = 'light';
-          }
-          document.documentElement.setAttribute('data-theme', theme);
-          localStorage.setItem('theme', theme);
-        });
+});
